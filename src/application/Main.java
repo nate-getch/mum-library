347425a48313e3d561af4b62af969d4344d8f90d
Natnael getachew <natwinet@gmail.com>
2017-07-03 17:10:52 -0500

    created login UI and folders

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,19 +1,23 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 
+import ui.Forms;
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
+			//BorderPane root = new BorderPane();
+			Parent root = FXMLLoader.load(getClass().getResource(Forms.LOGIN));
 			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Library Management System");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
---
+++ src/ui/Forms.java
@@ -0,0 +1,5 @@
+package ui;
+
+public class Forms {
+	public static String LOGIN = "/ui/Login.fxml";
+}
